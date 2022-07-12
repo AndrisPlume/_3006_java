4d84f385f852675870dda110672db16db2c69c4e
AndrisPlume <35973312+AndrisPlume@users.noreply.github.com>
2022-07-12 12:20:46 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+// correct login data
+        String epasts = "rcs@gmail.com";
+        String pass = "1234";
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Ludzu Ievadi epastu: ");
+        String userName = scan.nextLine();
+        System.out.println("Ludzu Ievadi paroli: ");
+        String userPass = scan.nextLine();
+
+        if (epasts.equals(userName)) {
+            if (pass.equals(userPass)) {
+                System.out.println("Sveiki, " + epasts + ", Tu esi ielogojies sistema");
+            } else {
+                System.out.println("parole vai epasts nav pareizs");
+            }
+        }
+        else {
+            System.out.println("parole vai epasts nav pareizs");
+        }
+
+    }
+}
